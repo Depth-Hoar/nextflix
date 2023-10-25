88ce016a1a53dbc0484a9ca6197e9f1d3884d943
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-10-25 06:24:00 -0600

    start off video page

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Card from "./card";
 import styles from "./section-cards.module.css";
 
@@ -8,9 +9,14 @@ const SectionCards = (props) => {
     <section className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
-      {videos.map((video, idx) => {
-        return <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />;
-      })}
+        {videos.map((video, idx) => {
+          console.log({ video });
+          return (
+            <Link key={idx} href={`/video/${video.Id}`}>
+              <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
