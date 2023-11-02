a349fbd6efc9007db999fd98f74a18c6d3a1afc9
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-11-02 06:23:01 -0600

    stats api

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,45 @@
+import jwt from "jsonwebtoken";
+import {
+  findVideoIdByUser,
+  insertStats,
+  updateStats,
+} from "../../lib/db/hasura";
+
 export default async function stats(req, resp) {
   if (req.method === "POST") {
-    resp.send({ msg: "it works" });
+    try {
+      const token = req.cookies.token;
+      if (!token) {
+        resp.status(403).send({});
+      } else {
+        const videoId = req.query.videoId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = decodedToken.issuer;
+        const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
+        if (doesStatsExist) {
+          // update it
+          const response = await updateStats(token, {
+            watched: true,
+            userId,
+            videoId,
+            favourited: 0,
+          });
+          resp.send({ msg: "it works", response });
+        } else {
+          // add it
+          const response = await insertStats(token, {
+            watched: false,
+            userId,
+            videoId,
+            favourited: 0,
+          });
+          resp.send({ msg: "it works", response });
+        }
+      }
+    } catch (error) {
+      console.error("Error occurred /stats", error);
+      resp.status(500).send({ done: false, error: error?.message });
+    }
   }
 }
